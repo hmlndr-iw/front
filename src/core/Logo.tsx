755f021a47b5a3fa2c733e9ae3ce3da66045a9d0
adamlahbib <adem.lahbib@insat.ucar.tn>
2023-01-22 10:20:06 +0100

    added logo

--- src/core/Logo.tsx
+++ src/core/Logo.tsx
@@ -1,3 +1,5 @@
 export default function Logo() {
-  return <h1 className="text-2xl text-tertiary-600 font-bold">DIGInvo</h1>
+  return <div className="flex ">
+    <img src="/art/logo.png" className="w-10 h-10 mr-5" />
+    <h1 className="mt-1 text-2xl text-tertiary-600 font-bold">DIGInvo</h1></div>
 }
